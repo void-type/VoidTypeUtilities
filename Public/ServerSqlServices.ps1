45a1b1e8d91b47cfe586d2ead584f50e1f631bcf
Jeff Schreiner <Jeff.S.Schreiner@gmail.com>
2019-10-01 10:42:37 -0600

    Move work utilities to be more generic

---
+++ Public/ServerSqlServices.ps1
@@ -0,0 +1,57 @@
+# TODO: add the names of all the sql services we care about here
+$private:services = "MSSQLSERVER", "SQLSERVERAGENT"
+
+function Get-ServerSqlServices {
+  <#
+  .SYNOPSIS
+  Checks all databases for non-running SQL Services.
+  Note that this only shows Windows Server 2012 and newer databases.
+
+  .EXAMPLE
+  PS C:\> Show-PrpaDbSqlServices
+  #>
+  [CmdletBinding()]
+  param(
+    # Names of computers to invoke against
+    [Parameter(
+      ValueFromPipeline = $true,
+      ValueFromPipelineByPropertyName = $true,
+      Position = 1,
+      Mandatory = $true
+    )]
+    [string[]]$ComputerName
+  )
+
+  return Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+    Get-Service |
+      Where-Object { $_.Name -in $services }
+  } |
+    Sort-Object -Property PSComputerName
+}
+
+function Start-ServerSqlServices {
+  <#
+  .SYNOPSIS
+  Starts all SQL Services on a server.
+
+  .EXAMPLE
+  PS C:\> Start-PrpaDbSqlServices -ComputerName AppDb
+  #>
+
+  [CmdletBinding()]param(
+    # Names of computers to invoke against
+    [Parameter(
+      ValueFromPipeline = $true,
+      ValueFromPipelineByPropertyName = $true,
+      Position = 1,
+      Mandatory = $true
+    )]
+    [string[]]$ComputerName
+  )
+
+  return Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+    Get-Service |
+      Where-Object { $_.Name -in $services } |
+      Start-Service
+  }
+}
---
+++ Public/WebCertificate.ps1
@@ -0,0 +1,95 @@
+function Get-WebCertificateExpiration {
+  <#
+    .SYNOPSIS
+    Query a site for certificate expiration.
+  .DESCRIPTION
+    Queries the site URL and gets the HTTPS certificate, then returns the
+    certificate expiration date.
+    .EXAMPLE
+    PS C:\> Show-WebCertificateExpiration -Url "Google.com"
+    #>
+  [CmdletBinding()]
+  param(
+    # The URLs to get certificates from.
+    [Parameter(
+      Position = 1,
+      Mandatory = $True,
+      ValueFromPipeline = $True,
+      ValueFromPipelineByPropertyName = $True
+    )]
+    [string[]]$Url,
+
+    [int]$Port = 443
+  )
+
+  process {
+
+    $certificates = Get-WebCertificate -Url $Url -Port $Port
+
+    foreach ($certificate in $certificates) {
+      [DateTime]$expirationDate = [System.DateTime]::Parse($certificate.GetExpirationDateString())
+      [int]$certExpiresIn = ($expirationDate - $(Get-Date -Hour 0 -Minute 0 -Second 0)).Days
+      $certName = $certificate.GetName()
+      $certIssuer = $certificate.GetIssuerName()
+
+      Write-Output [PSCustomObject]@ {
+        Url            = $Url;
+        Name           = $certName;
+        Issuer         = $certIssuer;
+        ExpirationDate = $expirationDate;
+        ExpiresInDays  = $certExpiresIn;
+      }
+    }
+  }
+}
+
+function Get-WebCertificate {
+  <#
+  .SYNOPSIS
+    Get the certificate of a website.
+  .DESCRIPTION
+    Queries the site URL and gets the HTTPS certificate.
+  .EXAMPLE
+    PS C:\> Get-Certificate -Url "Google.com"
+  #>
+  [CmdletBinding()]
+  param(
+    # The URLs to get certificates from.
+    [Parameter(
+      Position = 1,
+      Mandatory = $True,
+      ValueFromPipeline = $True,
+      ValueFromPipelineByPropertyName = $True
+    )]
+    [string[]]$Url,
+
+    [int]$Port = 443
+  )
+
+  begin {
+    $TCPClient = New-Object -TypeName System.Net.Sockets.TCPClient
+  }
+
+  process {
+    foreach ($webSite in $Url) {
+      $TcpSocket = New-Object Net.Sockets.TcpClient($webSite, $Port)
+      $tcpstream = $TcpSocket.GetStream()
+      $Callback = { param($sender, $cert, $chain, $errors) return $true }
+      $SSLStream = New-Object -TypeName System.Net.Security.SSLStream -ArgumentList @($tcpstream, $True, $Callback)
+
+      try {
+        $SSLStream.AuthenticateAsClient($webSite)
+        $Certificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($SSLStream.RemoteCertificate)
+      } finally {
+        $SSLStream.Dispose()
+      }
+
+      Write-Output $Certificate
+    }
+  }
+
+  end {
+    $TCPClient.Dispose()
+  }
+
+}
