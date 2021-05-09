66a6c157f42043a460b89ef5d412f1807f5be271
Jeff Schreiner <Jeff.S.Schreiner@gmail.com>
2021-05-09 17:58:14 -0600

    Improve dotnet refactorings

--- Public/DotnetWebApp.ps1
+++ Public/DotnetWebApp.ps1
@@ -44,26 +44,31 @@ function Find-DotnetClassFileNameMismatch {
     [string]$Path = "./"
   )
 
-  Get-ChildItem -Include *.cs -Recurse -Path $Path |
+  Get-ChildItem -Recurse -Include *.csproj -Path $Path |
+    Select-Object -ExpandProperty DirectoryName |
+    Get-ChildItem -Exclude obj, bin |
     ForEach-Object {
-      [Microsoft.PowerShell.Commands.MatchInfo[]]$matches = $_ | Select-String -Pattern 'public\s(\w*\s)*class'
+      Get-ChildItem -Path $_.FullName -Recurse *.cs |
+        ForEach-Object {
+          [Microsoft.PowerShell.Commands.MatchInfo[]]$matches = $_ | Select-String -Pattern '^\s*(public|internal|private)\s(\w*\s)*class'
 
-      if ($matches.Length -gt 0) {
-        $match = $matches[0]
+          if ($matches.Length -gt 0) {
+            $match = $matches[0]
 
-        if ($null -ne $match) {
-          $class = $match.Line.Split('class')[1].Split(' : ')[0].Split('<')[0].Split('{')[0].Trim()
-          $file = $match.filename.split('.')[0]
+            if ($null -ne $match) {
+              $class = $match.Line.Split('class')[1].Split(' : ')[0].Split('<')[0].Split('{')[0].Trim()
+              $file = $match.Filename.split('.')[0]
 
-          if ($class -ne $file) {
-            [PSCustomObject]@{
-              FileName  = $file
-              ClassName = $class
+              if ($class -ne $file) {
+                [PSCustomObject]@{
+                  FileName  = $match.Path
+                  ClassName = $class
+                }
+              }
             }
           }
         }
       }
-    }
 }
 
 function Find-DotnetNamespaceFileNameMismatch {
@@ -98,7 +103,10 @@ function Find-DotnetNamespaceFileNameMismatch {
           $path = $_.Directory.FullName.Replace('\', '.');
 
           if (-not $path.Contains($ns)) {
-            $_.FullName
+            [PSCustomObject]@{
+              FileName  = $_.FullName
+              Namespace = $ns
+            }
           }
         }
       }
