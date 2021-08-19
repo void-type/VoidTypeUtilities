aa26e38c4110106315a6f32965cbcb8d0e969e9e
Jeff Schreiner <Jeff.S.Schreiner@gmail.com>
2021-08-18 22:30:43 -0600

    Revamp WOL

--- Public/Send-Wol.ps1
+++ Public/Send-Wol.ps1
@@ -2,38 +2,45 @@ function Send-Wol {
   <#
     .SYNOPSIS
     Send a Wake-On-Lan magic packet to a broadcast address
-    .PARAMETER Mac
+    .PARAMETER MAC
     The MAC address of the device to wake
-    .PARAMETER Ip
+    .PARAMETER IpAddress
     The IP address to send the WOL packet to. This has a default of broadcast 255.255.255.255
     .PARAMETER Port
     The UDP port to send the WOL packet to. This has a default of port 9
     .EXAMPLE
     Send-Wol AAAAAAAAAAAA
-    Shortest possible command
+    Shortest possible command. Sends the packet 3 times.
     .EXAMPLE
-    Send-Wol -Mac AA:AA:AA:AA:AA:AA -Ip 192.168.1.5 -Port 7
-    Define the specific IP and Port, typically not needed.
+    Send-Wol -Mac AA:AA:AA:AA:AA:AA -IpAddress 192.168.1.5 -Port 7 -Repeat 7
+    Define the specific IP and Port, typically not needed. Will repeat the command 7 times.
     #>
   [CmdletBinding()]
   param(
     [Parameter(Mandatory = $True, Position = 1, ValueFromPipeline = $True)]
     [ValidatePattern('^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$')]
-    [string]$Mac,
-    [string]$Ip = "255.255.255.255",
-    [int]$Port = 9
+    [string]$MAC,
+    [string]$IpAddress = '255.255.255.255',
+    [int]$Port = 9,
+    [ValidateRange(1, [int]::MaxValue)]
+    [int]$Repeat = 3,
+    [ValidateRange(1, [int]::MaxValue)]
+    [int]$SleepMilliseconds = 150
   )
 
-  $broadcast = [Net.IPAddress]::Parse($Ip)
+  $MACBytes = $MAC.ToUpper() -split '[:-]' | ForEach-Object { [Convert]::ToByte($_, 16) }
 
-  $Mac = $Mac.ToUpper()
-  $macOut = $Mac -replace "[:-]"
-  $target = 0, 2, 4, 6, 8, 10 | ForEach-Object { [Convert]::ToByte($macOut.Substring($_, 2), 16) }
-  $packet = (, [byte]255 * 6) + ($target * 16)
+  # Magic packet is 6 bytes of 255, then the target's 6 byte MAC repeated 16 times.
+  $magicPacket = (, [byte]255 * 6) + ($MACBytes * 16)
 
   $UDPclient = New-Object System.Net.Sockets.UdpClient
-  $UDPclient.Connect($broadcast, $Port)
-  [void]$UDPclient.Send($packet, 102)
+  $UDPclient.Connect([Net.IPAddress]::Parse($IpAddress), $Port)
 
-  Write-Output "A magic packet was sent to $($Ip):$($Port) to wake device $($Mac)"
+  for ($i = 0; $i -lt $Repeat; $i++) {
+    $UDPclient.Send($magicPacket, $magicPacket.Length) | Out-Null
+    Write-Output "A magic packet was sent to $($IpAddress):$($Port) to wake device $($MAC)"
+    Start-Sleep -Milliseconds $SleepMilliseconds
+  }
+
+  $UDPclient.Dispose()
 }
