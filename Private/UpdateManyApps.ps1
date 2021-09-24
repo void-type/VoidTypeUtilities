70ac15a8eb1f7e545701f97fef7fb905ba43c3fc
Jeff Schreiner <Jeff.S.Schreiner@gmail.com>
2021-09-23 22:48:30 -0600

    Add bulk app updater

---
+++ Private/UpdateManyApps.ps1
@@ -0,0 +1,51 @@
+# This updates a whole folder of .NET applications.
+
+Push-Location "~/_gitLocal/_net5"
+
+# Try the upgrade
+Get-ChildItem | ForEach-Object -Parallel {
+  Push-Location $_
+  Write-Host "Working on $_"
+
+  git pull
+
+  try {
+    Push-Location -Path "src/*Web/ClientApp" -ErrorAction Stop
+    npm install
+    npm audit fix
+    npm update
+  } catch {
+    Write-Warning "Skipping client app"
+  } finally {
+    Pop-Location
+  }
+
+  dotnet tool restore
+  .\build\updateTools.ps1
+  dotnet outdated -u
+
+  Pop-Location
+}
+
+Write-Host "`n`n`n`n`n"
+
+# Double check when all done
+Get-ChildItem | ForEach-Object {
+  Push-Location $_
+  Write-Host "`n`nWorking on $_"
+
+  try {
+    Push-Location -Path "src/*Web/ClientApp" -ErrorAction Stop
+    npm outdated
+  } catch {
+    Write-Warning "Skipping client app"
+  } finally {
+    Pop-Location
+  }
+
+  dotnet outdated
+
+  Pop-Location
+}
+
+Pop-Location
