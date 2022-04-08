a7f4c0cc63ddeee56f681297cb8144d5b8a4394c
Jeff Schreiner <Jeff.S.Schreiner@gmail.com>
2022-04-07 18:23:18 -0600

    Allow changing prompt color

--- Private/Prompt.ps1
+++ Private/Prompt.ps1
@@ -1,11 +1,12 @@
 # Use by adding the following to your Profile
 # . "$($env:PSModulePath.Split(";")[0])/VoidTypeUtilities/Private/Prompt.ps1"
 function prompt {
+  $color = if ($null -ne $VC_PreferredPromptColor) { $VC_PreferredPromptColor } else { 'Cyan' }
   $dateString = Get-Date -UFormat '+%Y-%m-%d %H:%M'
   $locationString = (Get-Item -Path (Get-Location) -Force).Name
   $promptString = "pwsh $dateString $locationString>"
-  Write-Host $promptString -NoNewline -ForegroundColor Cyan
+  Write-Host $promptString -NoNewline -ForegroundColor $color
   return " "
 }
 
-Set-PSReadlineOption -BellStyle None
+Set-PSReadLineOption -BellStyle None
