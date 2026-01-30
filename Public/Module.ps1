efe84ac3c1f8fdc336f57c5c114aeabad82ec58e
Jeff Schreiner <jschreiner@bluemodus.com>
2026-01-30 09:16:18 -0700

    Daily updater

--- Public/Module.ps1
+++ Public/Module.ps1
@@ -6,6 +6,34 @@ function Update-VoidTypeUtilities {
   git -C "$((Get-Item $profile).Directory)\Modules\VoidTypeUtilities" pull
 }
 
+function Update-VoidTypeUtilitiesDaily {
+  $vtuUpdateFile = "$env:TEMP\VoidTypeUtilities_LastUpdate.txt"
+  $shouldUpdate = $false
+
+  if (Test-Path $vtuUpdateFile) {
+    $lastUpdate = Get-Content $vtuUpdateFile -ErrorAction SilentlyContinue
+    if ($lastUpdate) {
+      $lastUpdateDate = [DateTime]::Parse($lastUpdate)
+      if ($lastUpdateDate.Date -lt (Get-Date).Date) {
+        $shouldUpdate = $true
+      }
+    }
+  } else {
+    $shouldUpdate = $true
+  }
+
+  if ($shouldUpdate) {
+    Write-Host "Updating VoidTypeUtilities..." -ForegroundColor Gray
+    try {
+      Update-VoidTypeUtilities -ErrorAction SilentlyContinue
+      (Get-Date).ToString() | Set-Content $vtuUpdateFile
+      Import-Module VoidTypeUtilities -Force
+    } catch {
+      # Silently continue if update fails
+    }
+  }
+}
+
 function Show-VoidTypeUtilitiesVersion {
   <#
   .SYNOPSIS
